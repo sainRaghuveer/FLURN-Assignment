0535530c2c73dec7c259cfa09f9997085fe03a9c
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-24 12:13:26 +0530

    responsivness

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -83,91 +83,92 @@ function DetailsPage() {
     }, 2000);
   }, [search])
 
-  return loading ? <Spinner size="xl" color='red.500' thickness='6px' speed='0.65s' /> : (<div>
-    <Input id={style["search-input"]} placeholder='search Pokemon...' onChange={(e) => setSearch(e.currentTarget.value)} />
-    <article className={style.pokemon}>
-      <div className={style["container"]}>
-        <aside className={style["pokemon-pic"]}>
-          <Image title={pokemon?.name + " front deafult"} src={pokemon?.sprites?.front_default} alt={pokemon?.name + " front deafult"} />
-          <Image title={pokemon?.name + " front shiny"} src={pokemon?.sprites?.front_shiny} alt={pokemon?.name + " front shiny"} />
-          <Image title={pokemon?.name + " back shiny"} src={pokemon?.sprites?.back_shiny} alt={pokemon?.name + " back shiny"} />
-          <Image title={pokemon?.name + " back default"} src={pokemon?.sprites?.back_default} alt={pokemon?.name + " back default"} />
-        </aside>
-        <div className={style["pokemon-details"]}>
-          <h1 style={{ fontSize: "25px", fontWeight:"bold" }}>{pokemon?.name} <span onClick={handleDoFavorite}>{favoriteList.includes(pokemon?.name) ? <FaBookmark /> : <FaRegBookmark />}</span></h1>
-          <div className={style["pokemon-tables"]}>
-            <Table>
-              <Tbody>
-                <Tr>
-                  <Th><bdi>ID</bdi></Th>
-                  <Td>{pokemon?.id}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Base Experience</bdi></Th>
-                  <Td>{pokemon?.base_experience}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Type</bdi></Th>
-                  <Td>
-                    {
-                      pokemon?.types?.map(type => type.type.name).join(", ")
-                    }
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>HP</bdi></Th>
-                  <Td>{pokemon?.stats && pokemon?.stats[0]?.base_stat}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Attack</bdi></Th>
-                  <Td>{pokemon?.stats && pokemon?.stats[1]?.base_stat}</Td>
-                </Tr>
-                <Tr>
-                  <Td><bdi>Defense</bdi></Td>
-                  <Td>{pokemon?.stats && pokemon?.stats[2]?.base_stat}</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-            <Table>
-              <Tbody>
-                <Tr>
-                  <Th><bdi>Special-Attack</bdi></Th>
-                  <Td>{pokemon?.stats && pokemon?.stats[3]?.base_stat}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Special-Defense</bdi></Th>
-                  <Td>{pokemon?.stats && pokemon?.stats[4]?.base_stat}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Speed</bdi></Th>
-                  <Td>{pokemon?.stats && pokemon?.stats[5]?.base_stat}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Abilities</bdi></Th>
-                  <Td>
-                    {
-                      pokemon?.abilities?.map(el => el.ability.name).join(", ")
-                    }
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Weight</bdi></Th>
-                  <Td>{pokemon?.weight}</Td>
-                </Tr>
-                <Tr>
-                  <Th><bdi>Height</bdi></Th>
-                  <Td>{pokemon?.height}</Td>
-                </Tr>
-              </Tbody>
-            </Table>
+  return loading ? <Spinner size="xl" color='red.500' thickness='6px' speed='0.65s' /> : (
+    <div className={style.parentContainer}>
+      <Input id={style["search-input"]} placeholder='search Pokemon...' onChange={(e) => setSearch(e.currentTarget.value)} />
+      <article className={style.pokemon}>
+        <div className={style["container"]}>
+          <aside className={style["pokemon-pic"]}>
+            <Image title={pokemon?.name + " front deafult"} src={pokemon?.sprites?.front_default} alt={pokemon?.name + " front deafult"} />
+            <Image title={pokemon?.name + " front shiny"} src={pokemon?.sprites?.front_shiny} alt={pokemon?.name + " front shiny"} />
+            <Image title={pokemon?.name + " back shiny"} src={pokemon?.sprites?.back_shiny} alt={pokemon?.name + " back shiny"} />
+            <Image title={pokemon?.name + " back default"} src={pokemon?.sprites?.back_default} alt={pokemon?.name + " back default"} />
+          </aside>
+          <div className={style["pokemon-details"]}>
+            <h1 style={{ fontSize: "25px", fontWeight: "bold" }}>{pokemon?.name} <span onClick={handleDoFavorite}>{favoriteList.includes(pokemon?.name) ? <FaBookmark /> : <FaRegBookmark />}</span></h1>
+            <div className={style["pokemon-tables"]}>
+              <Table>
+                <Tbody>
+                  <Tr>
+                    <Th><bdi>ID</bdi></Th>
+                    <Td>{pokemon?.id}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Base Experience</bdi></Th>
+                    <Td>{pokemon?.base_experience}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Type</bdi></Th>
+                    <Td>
+                      {
+                        pokemon?.types?.map(type => type.type.name).join(", ")
+                      }
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>HP</bdi></Th>
+                    <Td>{pokemon?.stats && pokemon?.stats[0]?.base_stat}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Attack</bdi></Th>
+                    <Td>{pokemon?.stats && pokemon?.stats[1]?.base_stat}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td><bdi>Defense</bdi></Td>
+                    <Td>{pokemon?.stats && pokemon?.stats[2]?.base_stat}</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Table>
+                <Tbody>
+                  <Tr>
+                    <Th><bdi>Special-Attack</bdi></Th>
+                    <Td>{pokemon?.stats && pokemon?.stats[3]?.base_stat}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Special-Defense</bdi></Th>
+                    <Td>{pokemon?.stats && pokemon?.stats[4]?.base_stat}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Speed</bdi></Th>
+                    <Td>{pokemon?.stats && pokemon?.stats[5]?.base_stat}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Abilities</bdi></Th>
+                    <Td>
+                      {
+                        pokemon?.abilities?.map(el => el.ability.name).join(", ")
+                      }
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Weight</bdi></Th>
+                    <Td>{pokemon?.weight}</Td>
+                  </Tr>
+                  <Tr>
+                    <Th><bdi>Height</bdi></Th>
+                    <Td>{pokemon?.height}</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={style["pokemon-moves"]}>
-        <p><bdi>Moves</bdi> {pokemon?.moves?.map(el => el.move.name).join(", ")}</p>
-      </div>
-    </article>
-  </div>
+        <div className={style["pokemon-moves"]}>
+          <p><bdi>Moves</bdi> {pokemon?.moves?.map(el => el.move.name).join(", ")}</p>
+        </div>
+      </article>
+    </div>
   )
 }
 
