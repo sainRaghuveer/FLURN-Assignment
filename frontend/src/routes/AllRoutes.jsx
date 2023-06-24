7d0bceabfa59e242a8f885079b17ed6303dcaa23
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-24 17:58:22 +0530

    Four o Four page added

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -4,6 +4,7 @@ import SearchPage from '../pages/SearchPage';
 import ListingPage from '../pages/ListingPage';
 import DetailsPage from '../pages/DetailsPage';
 import BookmarksPage from '../pages/BookmarksPage';
+import FourOFour from '../pages/FourOFour';
 
 const AllRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AllRoutes = () => {
         <Route path="/list" element={<ListingPage />} />
         <Route path="/pokemon/:pokemonName" element={<DetailsPage />} />
         <Route path="/bookmarks" element={<BookmarksPage />} />
+        <Route path='*' element={<FourOFour/>}></Route>
       </Routes>
     </div>
   );
